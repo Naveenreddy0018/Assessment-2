3d552aacafd0a7cdbd1f30274fbe64a1e2d28bac
Naveen Kumar Reddy Chinthakunta <89237138+Naveenreddy0018@users.noreply.github.com>
2023-05-07 13:07:33 +0530

    Create Assessment 07-05-23.sql

---
+++ Assessment 07-05-23.sql
@@ -0,0 +1,15 @@
+Exercise 1 (5 marks):
+
+Create a database schema for the online consultation and therapy website. The database should contain the following tables:-
+
+Doctor table:- The table should contain the attributes such as id, name, specialization, etc.
+
+Appointments table:- The table should contain information related to appointments set up by doctors and patients.
+
+Patient table:- The table should contain information on patients.
+
+Reviews table:- This table should contain reviews posted by patients.
+
+
+NOTE:- Use proper constraints on the attributes such as primary key, not null and etc.
+
